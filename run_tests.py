e74ddd28882b772c1e768c49507c4153c0b7f1de
bx2k <BillXu2000@126.com>
2022-10-11 17:47:34 +0800

    robust for path

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,10 @@
-import subprocess, argparse
+import subprocess, argparse, os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--arch', default='gpu')
 args = parser.parse_args()
 
-subprocess.run(f'cd vertex_normal; python3 normal.py --test --arch {args.arch}', shell=True, check=True)
+paths = os.path.split(os.path.abspath(__file__))
+normal_path = os.path.join(paths[0], 'vertex_normal')
+
+subprocess.run(f'cd {normal_path}; python3 normal.py --test --arch {args.arch}', shell=True, check=True)
