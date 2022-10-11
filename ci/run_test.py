11c87a995eccb9afa3bdd5ea37519e349b2986eb
bx2k <BillXu2000@126.com>
2022-10-11 18:07:00 +0800

    switch to ci folder

--- ci/run_test.py
+++ ci/run_test.py
@@ -5,6 +5,6 @@ parser.add_argument('--arch', default='gpu')
 args = parser.parse_args()
 
 paths = os.path.split(os.path.abspath(__file__))
-normal_path = os.path.join(paths[0], 'vertex_normal')
+normal_path = os.path.join(paths[0], '..', 'vertex_normal')
 
 subprocess.run(f'cd {normal_path}; python3 normal.py --test --arch {args.arch}', shell=True, check=True)
